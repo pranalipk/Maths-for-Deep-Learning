71021ea18e889a9380fa1d27288c3d26818e052c
pranalipk <75980514+pranalipk@users.noreply.github.com>
2024-05-22 18:55:18 -0400

    Update regression_test.py

--- regression_test.py
+++ regression_test.py
@@ -2,6 +2,7 @@ import torch
 import pytest
 from regression import fit_regression_model
 
+
 def get_train_data(dim=1):
     """
     dim is the number of features in the input. for our purposes it will be either 1 or 2.
@@ -26,7 +27,7 @@ def get_train_data(dim=1):
          [24.,  5.],
          [19.,  7.],
          [14.,  4.],
-         [20.,  3.]])
+         [20.,  3.]], dtype=torch.float32)
     y = torch.tensor(
         [[1422.4000],
          [1469.5000],
@@ -47,45 +48,54 @@ def get_train_data(dim=1):
          [1523.1000],
          [1297.4000],
          [946.4000],
-         [1197.1000]])
+         [1197.1000]], dtype=torch.float32)
+    
     if dim == 1:
         X = X_2[:, :1]
     elif dim == 2:
         X = X_2
     else:
         raise ValueError("dim must be 1 or 2")
+    
     return X, y
+
 
 def test_fit_regression_model_1d():
     X, y = get_train_data(dim=1)
     model, loss = fit_regression_model(X, y)
     print(loss)
-    assert loss.item() < 4321, " loss too big"
+
+    assert loss.item() < 4321, "loss too big"
+
 
 def test_fit_regression_model_2d():
     X, y = get_train_data(dim=2)
     model, loss = fit_regression_model(X, y)
-    assert loss.item() < 400
+    assert loss.item() < 400, "loss too big"
+
 
 def test_fit_and_predict_regression_model_1d():
     X, y = get_train_data(dim=1)
     model, loss = fit_regression_model(X, y)
-    X_test = torch.tensor([[20.], [15.], [10.]])
+    X_test = torch.tensor([[20.], [15.], [10.]], dtype=torch.float32)
     y_pred = model(X_test)
     assert ((y_pred - torch.tensor([[1252.3008],
                                     [939.9971],
-                                    [627.6935]])).abs() < 2).all(), " y_pred is not correct"
-    assert y_pred.shape == (3, 1), " y_pred shape is not correct"
+                                    [627.6935]], dtype=torch.float32)).abs() < 2).all(), "y_pred is not correct"
+    assert y_pred.shape == (3, 1), "y_pred shape is not correct"
+
 
 def test_fit_and_predict_regression_model_2d():
     X, y = get_train_data(dim=2)
     model, loss = fit_regression_model(X, y)
-    X_test = torch.tensor([[20., 2.], [15., 3.], [10., 4.]])
+    X_test = torch.tensor([[20., 2.], [15., 3.], [10., 4.]], dtype=torch.float32)
     y_pred = model(X_test)
+
     assert ((y_pred - torch.tensor([[1191.9037],
                                     [943.9369],
-                                    [695.9700]])).abs() < 2).all(), " y_pred is not correct"
-    assert y_pred.shape == (3, 1), " y_pred shape is not correct"
+                                    [695.9700]], dtype=torch.float32)).abs() < 2).all(), "y_pred is not correct"
+    assert y_pred.shape == (3, 1), "y_pred shape is not correct"
+
 
 if __name__ == "__main__":
     test_fit_regression_model_1d()
